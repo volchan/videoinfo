2bfa3e42315b6db22cd19c61717f1b7cea489fa6
stephane de la torre <s.dlt@hotmail.fr>
2018-03-11 21:01:21 +0100

    session 2/17

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log("Video duration is: ", metadata.format.duration);
+  });
 });
